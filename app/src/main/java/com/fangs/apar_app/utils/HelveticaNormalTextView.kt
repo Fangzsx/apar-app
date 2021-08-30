a003fdfe1cf4c390c455280e2fa3ca69f4bbfc29
Fangszx <jygrzn@gmail.com>
2021-08-30 19:18:35 +0800

    add normal helvetica

--- app/src/main/java/com/fangs/apar_app/utils/HelveticaNormalTextView.kt
+++ app/src/main/java/com/fangs/apar_app/utils/HelveticaNormalTextView.kt
@@ -1,4 +1,17 @@
 package com.fangs.apar_app.utils
 
-class HelveticaNormalTextView {
+import android.content.Context
+import android.graphics.Typeface
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+
+class HelveticaNormalTextView(context : Context, attrs : AttributeSet) : AppCompatTextView(context, attrs) {
+
+    init{
+        applyFont()
+    }
+
+    private fun applyFont() {
+        val typeface = Typeface.createFromAsset(context.assets, "helvetica_normal")
+    }
 }
