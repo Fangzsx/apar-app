e83493f7da6ebaa95737c7c611c909be3cdf73c9
Fangszx <jygrzn@gmail.com>
2021-09-01 19:42:16 +0800

    customize top app bar

--- app/src/main/java/com/fangs/apar_app/activities/LoginActivity.kt
+++ app/src/main/java/com/fangs/apar_app/activities/LoginActivity.kt
@@ -1,14 +1,25 @@
 package com.fangs.apar_app.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.fangs.apar_app.R
+import com.fangs.apar_app.utils.HelveticaCustomButton
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val loginBtn = findViewById<HelveticaCustomButton>(R.id.btn_login)
+
+        loginBtn.setOnClickListener {
+            Intent(this,MainActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+
     }
 
 }
