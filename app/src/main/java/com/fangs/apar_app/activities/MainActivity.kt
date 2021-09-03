96e538695794f8ba0b5de4be247d4f64d7e2a848
Fangszx <jygrzn@gmail.com>
2021-09-03 19:09:54 +0800

    add side nav

--- app/src/main/java/com/fangs/apar_app/activities/MainActivity.kt
+++ app/src/main/java/com/fangs/apar_app/activities/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.fangs.apar_app.activities
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import com.fangs.apar_app.R
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val toolbar = findViewById<MaterialToolbar>(R.id.nav_drawer)
+        val drawer = findViewById<DrawerLayout>(R.id.drawer)
+        toolbar.setNavigationOnClickListener {
+            drawer.openDrawer(GravityCompat.START)
+        }
     }
 }
