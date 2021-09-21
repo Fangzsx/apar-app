df4dcec2cc0b3f34d3e03a11cf5195ab945109ed
Fangszx <jygrzn@gmail.com>
2021-09-21 20:32:16 +0800

    add button for new and old member purchase

--- app/src/main/java/com/fangs/apar_app/utils/HelveticaCustomButton.kt
+++ app/src/main/java/com/fangs/apar_app/utils/HelveticaCustomButton.kt
@@ -3,7 +3,6 @@ package com.fangs.apar_app.utils
 import android.content.Context
 import android.graphics.Typeface
 import android.util.AttributeSet
-import androidx.appcompat.widget.AppCompatButton
 import com.google.android.material.button.MaterialButton
 
 class HelveticaCustomButton(context : Context, attrs : AttributeSet) : MaterialButton(context, attrs){
@@ -13,7 +12,7 @@ class HelveticaCustomButton(context : Context, attrs : AttributeSet) : MaterialB
     }
 
     private fun applyFont() {
-        val typeface = Typeface.createFromAsset(context.assets, "helvetica_normal.ttf")
+        val typeface = Typeface.createFromAsset(context.assets, "helvetica_bold.ttf")
         setTypeface(typeface)
     }
 }
