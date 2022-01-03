f83849dc556e4fe7f55f56b0f8c4096faa971bd7
jay <jygrzn@gmail.com>
2022-01-03 10:27:38 +0800

    add purchase activity

---
+++ app/src/main/java/com/fangs/apar_app/activities/PurchaseActivity.kt
@@ -0,0 +1,12 @@
+package com.fangs.apar_app.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fangs.apar_app.R
+
+class PurchaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_purchase)
+    }
+}
