135bbbee86357bc30ef580e1e8d57826fe442879
jay <jygrzn@gmail.com>
2022-01-05 16:27:18 +0800

    display customer info in purchase activity

--- app/src/main/java/com/fangs/apar_app/activities/PurchaseActivity.kt
+++ app/src/main/java/com/fangs/apar_app/activities/PurchaseActivity.kt
@@ -13,14 +13,23 @@ class PurchaseActivity : AppCompatActivity() {
         binding = ActivityPurchaseBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
-        
+
         val intent = intent
         val lastname = intent.getStringExtra("LAST_NAME")
         val firstname = intent.getStringExtra("FIRST_NAME")
         val middlename = intent.getStringExtra("MIDDLE_NAME")
         val fullname = "$lastname, $firstname $middlename"
 
+        val houseST = intent.getStringExtra("HOUSE_ST")
+        val phaseZone = intent.getStringExtra("PHASE_ZONE")
+        val city = intent.getStringExtra("CITY")
+        val fullAddress = "$houseST, $phaseZone $city"
+
+        val birthday = intent.getStringExtra("BIRTHDAY")
+
         binding.tvCustomerName.text = fullname
+        binding.tvCustomerAddress.text = fullAddress
+        binding.tvCustomerBirthday.text = birthday
 
 
 
