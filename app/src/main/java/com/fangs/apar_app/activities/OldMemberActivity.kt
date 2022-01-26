e3b2d5b8b30e24547bb5024dad7980567976b780
jay <jygrzn@gmail.com>
2022-01-26 14:20:27 +0800

    add custom dialog for old member info

--- app/src/main/java/com/fangs/apar_app/activities/OldMemberActivity.kt
+++ app/src/main/java/com/fangs/apar_app/activities/OldMemberActivity.kt
@@ -1,10 +1,13 @@
 package com.fangs.apar_app.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.AlertDialog
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import com.fangs.apar_app.databinding.ActivityOldMemberBinding
+import com.fangs.apar_app.model.OldMember
 
-class OldMemberActivity : AppCompatActivity() {
+class OldMemberActivity : BaseActivity() {
     private lateinit var binding : ActivityOldMemberBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,8 +15,44 @@ class OldMemberActivity : AppCompatActivity() {
         binding = ActivityOldMemberBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btnPurchaseOld.setOnClickListener {
+            if(validateName()){
+                val lastname = binding.etOldMemberLastname.text.toString().uppercase()
+                val firstname = binding.etOldMemberFirstname.text.toString().uppercase()
+
+                //show dialog
+                val alertDialog = AlertDialog.Builder(this)
+                alertDialog.setTitle("Old Member")
 
 
+                OldMember.lastName = lastname
+                OldMember.firstName = firstname
 
+                Intent(this, OldMemberPurchaseActivity::class.java).also {
+                    startActivity(it)
+                }
+            }
+        }
+
+    }
+
+    private fun validateName() : Boolean {
+        val etLastName = binding.etOldMemberLastname.text?.trim()
+        val etFirstName = binding.etOldMemberFirstname.text?.trim()
+
+        return when{
+            TextUtils.isEmpty(etLastName) ->{
+                showErrorSnackBar(binding.root, "Last Name cannot be empty", true)
+                false
+            }
+
+            TextUtils.isEmpty(etFirstName) ->{
+                showErrorSnackBar(binding.root, "First Name cannot be empty", true)
+                false
+            }
+
+            else -> true
+
+        }
     }
 }
