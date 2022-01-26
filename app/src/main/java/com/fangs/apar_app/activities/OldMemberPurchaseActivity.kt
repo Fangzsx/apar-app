fb44dabe8c2a0290ee5611b4179e0a0c0c6aacf8
jay <jygrzn@gmail.com>
2022-01-26 14:03:04 +0800

    add layout for old member

---
+++ app/src/main/java/com/fangs/apar_app/activities/OldMemberPurchaseActivity.kt
@@ -0,0 +1,16 @@
+package com.fangs.apar_app.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fangs.apar_app.databinding.ActivityOldMemberPurchaseBinding
+
+class OldMemberPurchaseActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityOldMemberPurchaseBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityOldMemberPurchaseBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
