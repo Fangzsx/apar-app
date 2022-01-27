de76375289f9d2110d7eb373d560cf11479bc21e
jay <jygrzn@gmail.com>
2022-01-27 19:26:45 +0800

    send request via email

--- app/src/main/java/com/fangs/apar_app/activities/LoginActivity.kt
+++ app/src/main/java/com/fangs/apar_app/activities/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.fangs.apar_app.activities
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
@@ -21,38 +22,62 @@ class LoginActivity : BaseActivity() {
         //check if user is already logged in
         val user = auth.currentUser
         if (user != null) {
-
-            Intent(this, MainActivity::class.java).also {
-                startActivity(it)
-                finish()
-            }
+            goToMainActivity()
         }
+
+        binding.tvSendRequest.setOnClickListener {
+            composeEmail("someemail@gmail.com", "REQUEST ACCOUNT")
+        }
+
         binding.btnLogin.setOnClickListener {
 
             val email = binding.etEmail.text.toString().trim()
             val password = binding.etPassword.text.toString().trim()
             hideKeyboard(currentFocus ?: View(this))
+            login(email, password, auth)
+        }
+    }
 
-            if (email.isNotEmpty() && password.isNotEmpty()) {
-                auth.signInWithEmailAndPassword(email, password)
-                    .addOnCompleteListener { task ->
+    private fun login(
+        email: String,
+        password: String,
+        auth: FirebaseAuth
+    ) {
+        if (email.isNotEmpty() && password.isNotEmpty()) {
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
 
-                        if (task.isSuccessful) {
-                            Intent(this, MainActivity::class.java).also {
-                                startActivity(it)
-                                finish()
-                            }
-                        } else {
-                            showErrorSnackBar(
-                                binding.root,
-                                "Account does not exist. Try again.",
-                                true
-                            )
-                        }
+                    if (task.isSuccessful) {
+                        goToMainActivity()
+                    } else {
+                        showErrorSnackBar(
+                            binding.root,
+                            "Account does not exist. Try again.",
+                            true
+                        )
                     }
-            } else {
-                showErrorSnackBar(binding.root, "Email and Password cannot be empty!", true)
-            }
+                }
+        } else {
+            showErrorSnackBar(binding.root, "Email and Password cannot be empty!", true)
+        }
+    }
+
+    private fun goToMainActivity() {
+        Intent(this, MainActivity::class.java).also {
+            startActivity(it)
+            finish()
+        }
+    }
+
+    private fun composeEmail(addresses : String, subject: String) {
+        val myEmail = "jygrzn@gmail.com"
+        val intent = Intent(Intent.ACTION_SENDTO);
+        intent.data = Uri.parse("mailto:" + myEmail); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent);
         }
     }
 }
