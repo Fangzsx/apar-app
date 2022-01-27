f09f106d9c2ef69f512542ed4ca6f003580d2976
jay <jygrzn@gmail.com>
2022-01-27 10:24:29 +0800

    change icon

--- app/src/main/java/com/fangs/apar_app/activities/SplashActivity.kt
+++ app/src/main/java/com/fangs/apar_app/activities/SplashActivity.kt
@@ -1,22 +1,25 @@
 package com.fangs.apar_app.activities
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 import com.fangs.apar_app.R
 
 class SplashActivity : AppCompatActivity() {
 
+    @SuppressLint("ObsoleteSdkInt")
     override fun onCreate(savedInstanceState: Bundle?) {
         supportActionBar?.hide()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         @Suppress("DEPRECATION")
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-            window.setDecorFitsSystemWindows(false)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            WindowCompat.setDecorFitsSystemWindows(window, false)
         } else {
             window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
         }
